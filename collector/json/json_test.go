9e164e5fabbe886e8c9e11277c287c418282936b
Pavel Shvedko <pshvedko@gmail.com>
2021-04-23 00:12:30 +0300

    add promoter test

---
+++ collector/json/json_test.go
@@ -0,0 +1,81 @@
+package json
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewPromoter(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Promoter
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Ok",
+			args: args{name: "../../metrics.json"},
+			want: &Promoter{m: map[string]interface{}{
+				"currencies": []interface{}{
+					map[interface{}]interface{}{"name": "usd", "value": 70.},
+					map[interface{}]interface{}{"name": "eur", "value": 80.5},
+					map[interface{}]interface{}{"name": "rur", "value": 1.},
+				}}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewPromoter(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewPromoter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPromoter() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPromoter_Promote(t *testing.T) {
+	type fields struct {
+		m map[string]interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   map[string]interface{}
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Ok",
+			fields: fields{m: map[string]interface{}{
+				"currencies": []interface{}{
+					map[interface{}]interface{}{"name": "usd", "value": 70.},
+					map[interface{}]interface{}{"name": "eur", "value": 80.5},
+					map[interface{}]interface{}{"name": "rur", "value": 1.},
+				}}},
+			want: map[string]interface{}{
+				"currencies": []interface{}{
+					map[interface{}]interface{}{"name": "usd", "value": 70.},
+					map[interface{}]interface{}{"name": "eur", "value": 80.5},
+					map[interface{}]interface{}{"name": "rur", "value": 1.},
+				}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Promoter{
+				m: tt.fields.m,
+			}
+			if got := p.Promote(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Promote() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
